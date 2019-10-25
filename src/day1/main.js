2e2ed13ac83a9f6ed045b7cc77b8e4f1539b63e4
koal4z <55755687+koal4z@users.noreply.github.com>
2019-10-26 04:03:20 +0700

    Update main.js

--- src/day1/main.js
+++ src/day1/main.js
@@ -1,28 +1,24 @@
 function main(str) {
 	// Your code begins here;
-	let splitstr = str.split('');
-	const base = {a:'(',
-				b:')',
-				c:'[',
-				d:']',
-				e:'{',
-				f:'}'}
-	let compstr = [];
-	//console.log(splitstr)
-	for (let i=0;i <splitstr.length; i++){
-		compstr +=  splitstr[i];
-	}
-	if (compstr === '([])[]({})'){
-		return true;
-	}else if (compstr === '([)]'){
-		return false;
-	}else if (compstr === "((()"){
-		return false;
-	}
-		console.log(compstr)
-	//console.log(typeof base.a);
-	let joinstr = splitstr.join('');
-	//console.log(joinstr)
+	let a = '('
+	let b = ')'
+	let c = '{'
+	let d = '}'
+	let e = '['
+	let f = ']'
+	//let splitstr = str.split('')
+	function i(x) {
+		if(x = '('){
+		return 1
+		}else {
+			return false
+		}
+	};
+
+	i(a);
+
+	console.log(a>1)
+	//console.log(str)
 }
 
 module.exports = { main };
