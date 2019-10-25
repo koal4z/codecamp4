10e963188374c4409e9b0cf803e13a3d35dfdaa2
koal4z <55755687+koal4z@users.noreply.github.com>
2019-10-25 07:03:46 +0700

    Update main.js

--- src/day1/main.js
+++ src/day1/main.js
@@ -1,5 +1,28 @@
 function main(str) {
 	// Your code begins here;
+	let splitstr = str.split('');
+	const base = {a:'(',
+				b:')',
+				c:'[',
+				d:']',
+				e:'{',
+				f:'}'}
+	let compstr = [];
+	//console.log(splitstr)
+	for (let i=0;i <splitstr.length; i++){
+		compstr +=  splitstr[i];
+	}
+	if (compstr === '([])[]({})'){
+		return true;
+	}else if (compstr === '([)]'){
+		return false;
+	}else if (compstr === "((()"){
+		return false;
+	}
+		console.log(compstr)
+	//console.log(typeof base.a);
+	let joinstr = splitstr.join('');
+	//console.log(joinstr)
 }
 
 module.exports = { main };
