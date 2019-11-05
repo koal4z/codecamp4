1c150ddddbf8f4454c4441ef3637de181de3e555
koal4z <55755687+koal4z@users.noreply.github.com>
2019-11-05 22:36:11 +0700

    day2 complete

--- src/day2/main.js
+++ src/day2/main.js
@@ -1,36 +1,21 @@
 function main(str, str2) {
 	// Your code begins here;
 
-	if (str.length === 3){
-		return s1(str, str2) ;
-	} else {
-		return s2(str, str2) ;
-	}
+let arrstr = [];
+let arrstr2 = [];
+let sum = 0;
+for (let i=0;i<str.length;i++){
+	arrstr.push(str[i]);
+	arrstr2.push(str2[i]);
+	if (arrstr[0] !== arrstr2[0]){
+		sum ++;
+	};
+	arrstr.shift(str[i]);
+	arrstr2.shift(str2[i]);
 
-	function s1(str, str2) {
-	if (str === str2) {
-		return 0;
-	} else if ((str[0] !== str2[0] && str[1] !== str2[1] && str[2] !== str2[2])){
-		return  3;
-	} else {
-		return 2;
-	}
-}
-function s2(str, str2) {
-	if (str === str2) {
-		return 0;
-}	else if (str[0] === str2[0] && str[1] !== str2[1] &&
-	str[2]!== str2[2] && str[3]!== str2[3] && str[4]!== str2[4] &&
-	str[5]!== str2[5] ) {
-		return 5;
-	} else if (str[0]!== str2[0] && str[1]!== str2[1] &&
-		str[2]!== str2[2] && str[3]!== str2[3] && str[4]!== str2[4] &&
-		str[5]!== str2[5] ) {
-			return 6;
-	} else {
-		return 2;
-	}
-}
+};
+return sum;
+
 }
 
 module.exports = { main };
