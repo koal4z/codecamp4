ff40f69eeca5a8bd2f71ea9a844c2f8e86d2bb6b
koal4z <55755687+koal4z@users.noreply.github.com>
2019-12-21 21:33:56 +0700

    update day4-5

--- src/day4/main.js
+++ src/day4/main.js
@@ -1,5 +1,25 @@
 function main(arr) {
-	// Your code begins here;
+  // Your code begins here;
+
+  if (arr.length === 1) {
+    return 1;
+  }
+  if (arr.length === 2 && arr[0][1] > arr[1][1]) {
+    return 2;
+  }
+  let start = [];
+  let out = [];
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      if (j === 0) {
+        start.push(arr[i][j]);
+      } else {
+        out.push(arr[i][j]);
+      }
+    }
+  }
+  let filterArr = start.filter(time => time > start[0] && time <= out[0]);
+  return filterArr.length + 1;
 }
 
 module.exports = { main };
--- src/day5/main.js
+++ src/day5/main.js
@@ -1,5 +1,24 @@
 function main(arr) {
-	// Your code begins here;
+  // Your code begins here;
+  let rArr = [];
+  let gArr = [];
+  let bArr = [];
+  let a = str => {
+    switch (str) {
+      case "R":
+        rArr.push(str);
+        break;
+      case "G":
+        gArr.push(str);
+        break;
+      case "B":
+        bArr.push(str);
+        break;
+    }
+  };
+  arr.map(str => a(str));
+  return rArr.concat(gArr).concat(bArr);
 }
 
 module.exports = { main };
+
